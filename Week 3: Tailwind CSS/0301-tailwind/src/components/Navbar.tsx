16aa5bd86a22306d22f1f05e24fcee6f5ff0ee76
amandahsieh <amandahsieh.926188@gmail.com>
2025-03-28 17:13:43 +0800

    <feature>: Navbar Component with Tailwind CSS

--- Week 3: Tailwind CSS/0301-tailwind/src/components/Navbar.tsx
+++ Week 3: Tailwind CSS/0301-tailwind/src/components/Navbar.tsx
@@ -1,15 +1,15 @@
-import "./Navbar.css";
-
 function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="logo">Logo</div>
-      <ul className="nav-list">
-        <li className="nav-item">Home</li>
-        <li className="nav-item">About</li>
-        <li className="nav-item">Contact</li>
+    <nav className="bg-gray-800 p-4 pl-8 pr-8 flex justify-between items-center">
+      <div className="text-white text-3xl font-bold mr-8">Logo</div>
+      <ul className="flex m-0 p-0 list-none space-x-10">
+        <li className="text-white hover:text-gray-400">Home</li>
+        <li className="text-white hover:text-gray-400">About</li>
+        <li className="text-white hover:text-gray-400">Contact</li>
       </ul>
-      <button className="login-btn">Login</button>
+      <button className="bg-blue-600 text-white p-2 rounded cursor-pointer hover:bg-blue-800 ml-8">
+        Login
+      </button>
     </nav>
   );
 };
