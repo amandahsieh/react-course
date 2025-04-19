d1c1961d156a3b0c4cc9c1c536950c1198b93271
amandahsieh <amandahsieh.926188@gmail.com>
2025-04-19 17:34:02 +0800

    <feature>: More Props - Due Date

--- Week 4: Props/0401-review/src/components/TodoItem.tsx
+++ Week 4: Props/0401-review/src/components/TodoItem.tsx
@@ -1,9 +1,10 @@
 import DeleteButton from './DeleteButton.tsx'
 
-function TodoItem({itemName='Unamed Todo Item'}) {
+function TodoItem({itemName='Unamed', dueDate='No Due Date'}) {
   return (
     <div className="flex justify-between items-center border-b py-2">
       <span className="text-gray-700">{itemName}</span>
+      <span className="text-sm text-gray-500">Due: {dueDate}</span>
       <DeleteButton />
     </div>
   )
