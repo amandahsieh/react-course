6c1289e79b6c63c0bf2eeb21fba701265eea6fb4
amandahsieh <amandahsieh.926188@gmail.com>
2025-04-21 09:26:11 +0800

    <feature>: Add Due Date

--- Week 4: Props/0401-todo/src/components/TodoItem.tsx
+++ Week 4: Props/0401-todo/src/components/TodoItem.tsx
@@ -1,9 +1,13 @@
 import DeleteButton from './DeleteButton.tsx'
 
-function TodoItem() {
+function TodoItem(
+	{ itemName='Unamed', dueDate='No Due Date' }:
+	{ itemName?: string, dueDate?: string }
+) {
   return (
     <div className="flex justify-between items-center border-b py-2">
-      <span className="text-gray-700">待辦事項</span>
+      <span className="text-gray-700">{itemName}</span>
+      <span className="text-sm text-gray-500">Due: {dueDate}</span>
       <DeleteButton />
     </div>
   )
