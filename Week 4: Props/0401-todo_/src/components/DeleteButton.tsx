1f35cbcbbe52cf54490586e5c86c1d5944d2a1b6
amandahsieh <amandahsieh.926188@gmail.com>
2025-04-21 18:26:23 +0800

    <Refactor>

---
+++ Week 4: Props/0401-todo_/src/components/DeleteButton.tsx
@@ -0,0 +1,8 @@
+function DeleteButton() {
+    return (
+        <button className="text-sm text-red-500 hover:underline">
+            Delete
+        </button>
+    )
+}
+export default DeleteButton
