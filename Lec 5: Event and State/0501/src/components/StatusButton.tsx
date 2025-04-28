21a58dedd53619d103fbf68498eae5a3429ccc12
amandahsieh <amandahsieh.926188@gmail.com>
2025-04-28 15:22:01 +0800

    <event>: Event Handson

--- Lec 5: Event and State/0501/src/components/StatusButton.tsx
+++ Lec 5: Event and State/0501/src/components/StatusButton.tsx
@@ -3,17 +3,34 @@ function StatusButton(
     { defaultStatus?: 'Not Started' | 'Progress' | 'Done' | 'Archived' | undefined }
 ) {
     const colorMap: Record<'Not Started' | 'Progress' | 'Done' | 'Archived', string> = {
-        'Not Started': 'bg-gray-200 text-gray-700',
+        'Not Started': 'bg-gray-300 text-gray-700',
         'Progress': 'bg-indigo-300 text-blue-800',
-        'Done': 'bg-green-200 text-green-950',
+        'Done': 'bg-lime-600 text-green-950',
         'Archived': 'bg-yellow-500 text-yellow-900',
     };
-    const colorClass = colorMap[defaultStatus] ?? 'bg-gray-200 text-gray-700';
 
+    const handleClick = (event: React.MouseEvent) => {
+        const button = event.currentTarget;
+        const currentStatus = button.textContent as 'Not Started' | 'Progress' | 'Done' | 'Archived';
+        const statusMap: Map<'Not Started' | 'Progress' | 'Done' | 'Archived', 'Not Started' | 'Progress' | 'Done' | 'Archived'> = new Map([
+        ['Not Started', 'Progress'],
+        ['Progress', 'Done'],
+        ['Done', 'Archived'],
+        ['Archived', 'Not Started']
+        ]);
+        const nextStatus = statusMap.get(currentStatus) || 'Not Started';
+        button.textContent = nextStatus;
+        button.className = `text-sm border w-28 rounded px-2 py-1 focus:outline-none ${colorMap[nextStatus]}`;
+    };
+    
     return (
-        <button className={`text-sm border w-28 rounded px-2 py-1 focus:outline-none ${colorClass}`}>
-            {defaultStatus}
+        <button
+        onClick={handleClick}
+        className={`text-sm border w-28 rounded px-2 py-1 focus:outline-none ${colorMap[defaultStatus]}`}
+        >
+        {defaultStatus}
         </button>
-    )
+    );
 }
-export default StatusButton
+
+export default StatusButton;
